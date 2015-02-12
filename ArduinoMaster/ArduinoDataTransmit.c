f5ecf46baa4524439215078321c148561e40fc6b
Ro5635 <Ro5635@gmail.com>
2015-02-12 23:13:51 +0000

    Added Arduno Data
    Added arduino data to the repo

---
+++ ArduinoMaster/ArduinoDataTransmit.c
@@ -0,0 +1,147 @@
+//The system is desined to operate at a selction of baud rates, these are:
+//1200,2400,4800,9600,19200,38400,57600,115200,230400
+int BaudRateSerial1 = 9600; //The BaudRate to use for Communication
+
+  boolean syncSuccess = false;  //Sync status
+  boolean syncTest = false; //This is used to compleate 3 way handshake
+  boolean FinishedInput = false;
+  String recivedtring = "";
+
+//PC Access Mode Var's
+boolean PCAccessMode = false; 
+
+void setup() {
+//Start the Serial Port
+  Serial.begin(BaudRateSerial1); 
+  //print welcome message
+  Serial.println("Arduino Robotic System");
+  Serial.println("www.weblink.co.uk");
+}
+
+
+void loop() {
+if (PCAccessMode){
+//PC Access Mode Enabled, 
+
+
+
+}else {
+//Standard Run
+
+
+}
+
+}//end loop
+
+//Read Data If Data is avalable
+void serialEvent(){
+
+  while (Serial.available()){
+
+    char recivedchar = (char)Serial.read();
+    if (recivedchar == '^') {
+      FinishedInput = true;
+    }
+    if (recivedchar != '^'){
+      recivedtring += recivedchar;
+    }
+  }
+
+   if (FinishedInput) {
+
+    coreCommunication();
+
+      if(PCAccessMode){
+
+        //PC Access mode enabled
+        //this is used because there will be a different command set to listen for compared to the standard set
+        PCAccessCommunication();
+
+        } else {
+
+        runTimeCommunication(); // standard runtime communicaation
+
+      }
+    
+    recivedtring = "";
+    FinishedInput = false;
+      } 
+    }
+
+    void coreCommunication(){
+
+
+      if(recivedtring == "Robot?"){
+        //Start Up Communication from Program
+        Serial.println("Arduino_Robot");
+        Serial.println("Entering PC Access Mode");
+        PCAccessMode = true;
+        } 
+
+    }
+
+    void PCAccessCommunication(){
+        
+        
+
+    }
+
+    void syncCommunication(){
+    //This is used to compleate a 3 way handshake before data transmit.
+
+      Serial.println("sync"); //Ask for sync
+
+      while(!syncSuccess) {
+          
+          serialEvent();
+
+          if (syncInProgress){
+
+          if (dataRecived == "syncAwk"){
+              syncSuccess = true;
+           }
+
+
+           } else {
+              //Start sync
+              syncSuccess = false;
+
+
+          }
+
+          if (recivedtring == "syncAwk" && syncTest){
+              syncSuccess; //Both points are listining, transmit ready.
+          }
+
+
+
+      }
+
+
+    
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
