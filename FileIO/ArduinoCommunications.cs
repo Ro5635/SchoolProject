25bdf6d16ea15003e20dbf3b64a0933f860a95fd
Ro5635 <Ro5635@gmail.com>
2015-03-24 18:46:12 +0000

    Created Arduino Commication Class
    Created Arduino Commication Class

--- FileIO/ArduinoCommunications.cs
+++ FileIO/ArduinoCommunications.cs
@@ -16,8 +16,37 @@ namespace FileIO
          */
 
         //The data Tables:
-        int[,] VarableStatus = new int[500,2];
-        string 
+
+        //Stored as two seperate arrays not multidimensioinal , objects for simplicity.
+        const int MaxVars = 500; //Max number of varables
+        int[] VarableStatus = new int[MaxVars]; // Status : 0 = set, posative = tallying parses throuth whilst waiting for data, -1: Not initialised this session.
+        string[] VarableData = new string[MaxVars]; // this contains the actual data as a string.
+
+        //Create Object SerialConnectionControl.
+        SerialConnectionControl SerialControler = new SerialConnectionControl();
+
+
+        //Class initilised?
+        Boolean Initialised = false;
+
+        private void initialisation()
+        {
+            //The pourpose of this class is to prepare the class for active function.
+            //this should be called when this class is first used automaticaly.
+
+            //Set all status to -1:
+            for (int i = 0; i < MaxVars; i++)
+            {
+                VarableStatus[i] = -1;
+            }
+
+
+
+                Initialised = true; //ensure that this is not run again.
+        }
+
+
+ 
 
 
     }
