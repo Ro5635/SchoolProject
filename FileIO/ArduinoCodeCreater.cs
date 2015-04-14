463063bba3df136dba64c7aa9a6a1cfa632c7a8a
Ro5635 <Ro5635@gmail.com>
2015-04-14 16:14:38 +0100

    Added Arduino Code Creator
    Arduino Code Creator Aded

---
+++ FileIO/ArduinoCodeCreater.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    class ArduinoCodeCreater
+    {
+
+        int SerialBaudRate;
+        string welcomeWebLink = "www.example.co.uk";
+
+
+        Boolean WriteToArduinoFileInitilised = false;
+        string ArduinoFileLocation = "NA";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test of plans to make this class");
+            //create the object if this class
+            ArduinoCodeCreater ArdCodeCreater = new ArduinoCodeCreater();
+
+            ArdCodeCreater.LoadData();
+            ArdCodeCreater.WriteToArduinoFile("", false);
+            ArdCodeCreater.FileHeader();
+
+            //Setup the universal parameters to all arduino robots.
+            ArdCodeCreater.UniversalSetup();
+
+
+
+
+
+        }
+
+
+        private void FileHeader()
+        {
+            WriteToArduinoFile("//This was created with the arduino code creation Tool.");
+            WriteToArduinoFile("//Please paste the bellow text into the arduino compiler and upload to your selected Arduino board.");
+        }
+
+        private void LoadData()
+        {
+
+            ArduinoFileLocation = @"";//@"/Users/robert/Documents/Example.ino";
+            //Update all of these by using the arduino robot file.
+
+            //Serial
+            SerialBaudRate = 9600;
+
+            //Micilanius Servos Knob Type
+            Boolean ServoMic1 = false;
+            Boolean ServoMic2 = false;
+
+            //Micilanius servos Sweep Type:
+            Boolean ServoSweep = false;
+            Boolean ServoSweep2 = false;
+
+            //Motors
+            //the arduino motor controller board is assumed to be available
+
+
+            //Servo as Drive
+            Boolean ServoDriveAsDrive = false;
+
+            //Sensors
+        }
+
+        private Boolean WriteToArduinoFile(string LineToWrite, Boolean Append = true)
+        {
+            //This method allows writing to the Arduino file.
+            //Write is one line at a time.
+
+            //Ensure that the File Location has been filled in.
+            if (ArduinoFileLocation == "NA")
+            {
+                //The file location has not been completed, return write fail.
+                return false;
+            }
+            else
+            {
+                //Write to the file
+
+                if (Append)
+                { //append or over write
+
+                    try
+                    {
+                        using (StreamWriter WriteHandle = new StreamWriter(ArduinoFileLocation, true))
+                        {
+                            WriteHandle.WriteLine(LineToWrite);
+                            return true;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("The file could not be Written to:");
+                        Console.WriteLine(e.Message);
+                        return false;
+                    }
+
+                }
+                else
+                {
+
+                    try
+                    {
+                        using (StreamWriter WriteHandle = new StreamWriter(ArduinoFileLocation, false)) //Overwrite File
+                        {
+                            WriteHandle.WriteLine(LineToWrite);
+                            return true;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("The file could not be Written to:");
+                        Console.WriteLine(e.Message);
+                        return false;
+                    }
+
+
+                }
+            }
+        }
+
+        private void UniversalSetup()
+        {
+            //This function runs the setup necessary for all arduino types.
+
+            //Start the Setup void
+            WriteToArduinoFile("void setup() {//Code to be ran one at power on");
+            SetupSerial(SerialBaudRate);
+            WriteToArduinoFile("}//End Setup void");//End Arduino Setup function
+
+
+        }
+        private void SetupSerial(int BaudRate)
+        {
+            //This method creates the serial port for the arduino to use.
+
+            WriteToArduinoFile("//The system is designed to operate at a selection of baud rates, these are:");
+            WriteToArduinoFile("//1200,2400,4800,9600,19200,38400,57600,115200,230400");
+            string LineToWrite = "int BaudRateSerial1 = " + BaudRate + "//The BaudRate to use for Communication";
+
+            WriteToArduinoFile(LineToWrite);
+            WriteToArduinoFile("Serial.begin(BaudRateSerial1);"); //Start the Serial interface
+            WriteToArduinoFile("//print the welcome message " + '\n' + "Serial.println(\"Arduino Robotic System\");" + '\n' + "Serial.println(" + welcomeWebLink + ");");
+
+
+        }
+
+
+    }
+}
